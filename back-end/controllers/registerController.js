dc3137af7cc374254b258ccccacdb6c8841445ec
virginia-duca <virginiambd.bio@gmail.com>
2021-03-08 09:59:33 -0300

    pacote supertest para back-end

--- back-end/controllers/registerController.js
+++ back-end/controllers/registerController.js
@@ -5,6 +5,10 @@ const registerService = require('../services/registerServices');
 const register = Router();
 
 register.post('/', rescue(async (req, res, next) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(401).json({ message: "field name is required"})
+  }
   const user = await registerService.create(req.body);
   if (user.error) {
     return next(user);
